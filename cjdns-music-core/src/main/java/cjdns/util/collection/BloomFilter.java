cd76b6c9551c38ed79fc6c0b44c586f4c690c799
cjdns-cc <cjdns.cc@gmail.com>
2013-07-02 20:03:36 +0400

    bloom filter

--- cjdns-music-core/src/main/java/cjdns/util/collection/BloomFilter.java
+++ cjdns-music-core/src/main/java/cjdns/util/collection/BloomFilter.java
@@ -1,5 +1,8 @@
 package cjdns.util.collection;
 
+import cjdns.cjdns_music.Model.Bloom;
+import com.google.protobuf.ByteString;
+
 import java.util.Arrays;
 import java.util.BitSet;
 
@@ -7,34 +10,42 @@ import java.util.BitSet;
  * User: willzyx
  * Date: 29.06.13 - 23:54
  */
-public abstract class BloomFilter<T> {
+public final class BloomFilter<T> {
+    public static interface Hash<T> {
+        public long get(T obj);
+    }
+
+    private final Hash<T> hash;
     private final int[] vector;
     private final BitSet bitset;
     private final int mask;
 
-    protected BloomFilter(int[] vector, BitSet bitset) {
-        this.vector = Arrays.copyOf(vector, vector.length);
-        this.bitset = bitset;
-        int size = bitset.size();
+    private BloomFilter(int[] vector, Hash<T> hash, byte[] bitset) {
+        int size = bitset.length * 8;
         assert ((size & (size - 1)) == 0);
+        this.hash = hash;
+        this.vector = Arrays.copyOf(vector, vector.length);
+        this.bitset = BitSet.valueOf(bitset);
         this.mask = size - 1;
     }
 
-    protected BloomFilter(int[] vector, int size) {
-        this(vector, new BitSet(size));
+    private BloomFilter(int[] vector, Hash<T> hash, int size) {
+        assert ((size & (size - 1)) == 0);
+        this.hash = hash;
+        this.vector = Arrays.copyOf(vector, vector.length);
+        this.bitset = new BitSet(size);
+        this.mask = size - 1;
     }
-
-    protected abstract long hash(T obj);
 
     public void add(T obj) {
         for (int k : vector) {
-            bitset.set((int) (hash(obj) * k) & mask);
+            bitset.set((int) (hash.get(obj) * k) & mask);
         }
     }
 
     public boolean contains(T obj) {
         for (int k : vector) {
-            if (!bitset.get((int) (hash(obj) * k) & mask)) {
+            if (!bitset.get((int) (hash.get(obj) * k) & mask)) {
                 return false;
             }
         }
@@ -47,6 +58,45 @@ public abstract class BloomFilter<T> {
 
     public byte[] getByteArray() {
         return bitset.toByteArray();
+    }
+
+    public Bloom getMessage() {
+        Bloom.Builder builder = Bloom.newBuilder();
+        for (int k : vector) {
+            builder.addFactor(k);
+        }
+        builder.setBitset(ByteString.copyFrom(bitset.toByteArray()));
+        return builder.build();
+    }
+
+    /* STATIC INTERFACE */
+
+    private final static Hash<Object> DEFAULT_HASH =
+            new Hash<Object>() {
+                @Override
+                public long get(Object obj) {
+                    return obj.hashCode();
+                }
+            };
+
+    public static BloomFilter<Object> restore(Bloom message) {
+        int[] vector = new int[message.getFactorCount()];
+        for (int i = 0; i < message.getFactorCount(); i++) {
+            vector[i] = message.getFactor(i);
+        }
+        return new BloomFilter<Object>(
+                vector,
+                DEFAULT_HASH,
+                message.getBitset().toByteArray()
+        );
+    }
+
+    public static BloomFilter<Object> getEmpty(int[] vector, int size) {
+        return new BloomFilter<Object>(
+                vector,
+                DEFAULT_HASH,
+                size
+        );
     }
 
 }
