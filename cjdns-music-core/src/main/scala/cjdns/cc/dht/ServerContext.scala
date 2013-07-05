4cd3aa4a53d52aad5a6ce032b882ad005ff43047
cjdns-cc <cjdns.cc@gmail.com>
2013-07-05 21:33:06 +0400

    server context

---
+++ cjdns-music-core/src/main/scala/cjdns/cc/dht/ServerContext.scala
@@ -0,0 +1,20 @@
+package cjdns.cc.dht
+
+import collection.mutable
+import concurrent.Promise
+import cjdns.cc.DHT
+
+/**
+ * User: willzyx
+ * Date: 05.07.13 - 21:02
+ */
+class ServerContext {
+
+  val connections = mutable.LinkedHashMap.empty[I, Connection]
+
+  class Query(val promise: Promise[DHT.Packet],
+              val timestamp: Long = System.currentTimeMillis)
+
+  val queries = mutable.LinkedHashMap.empty[(I, Long), Query]
+
+}
