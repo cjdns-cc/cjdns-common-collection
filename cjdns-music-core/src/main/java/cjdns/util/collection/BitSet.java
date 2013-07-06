5374c76b35bd3a33e3fd90748f4cf0c632efbe07
cjdns-cc <cjdns.cc@gmail.com>
2013-07-07 01:04:55 +0400

    bitset

--- cjdns-music-core/src/main/java/cjdns/util/collection/BitSet.java
+++ cjdns-music-core/src/main/java/cjdns/util/collection/BitSet.java
@@ -1,5 +1,7 @@
 package cjdns.util.collection;
 
+import java.util.Arrays;
+
 /**
  * User: willzyx
  * Date: 06.07.13 - 22:54
@@ -13,12 +15,25 @@ public final class BitSet {
         this.words = new long[((length - 1) >> 6) + 1];
     }
 
+    private BitSet(int length, long[] words) {
+        this.length = length;
+        this.words = words;
+    }
+
     public void set(int i) {
         words[i >> 6] |= (1L << i);
     }
 
     public boolean get(int i) {
         return (words[i >> 6] & (1L << i)) != 0;
+    }
+
+    public byte[] toByteArray() {
+        byte[] buffer = new byte[((length - 1) >> 3) + 1];
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = (byte) (words[i >> 3] >>> ((i & 7) << 3));
+        }
+        return buffer;
     }
 
     /* */
@@ -42,12 +57,12 @@ public final class BitSet {
         return bits;
     }
 
-    public byte[] toByteArray() {
-        byte[] buffer = new byte[((length - 1) >> 3) + 1];
-        for (int i = 0; i < buffer.length; i++) {
-            buffer[i] = (byte)(words[i >> 3] >>> ((i & 7) << 3));
-        }
-        return buffer;
+    public static BitSet valueOf(BitSet bitset) {
+        return valueOf(bitset, bitset.length);
+    }
+
+    public static BitSet valueOf(BitSet bitset, int length) {
+        return new BitSet(length, Arrays.copyOf(bitset.words, ((length - 1) >> 6) + 1));
     }
 
 }
